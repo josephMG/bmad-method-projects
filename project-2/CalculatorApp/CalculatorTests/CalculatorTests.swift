10e11a22aadc22d0e631b4409ec529c5e1925067
JosephMG <joseph780116.mg@gmail.com>
2025-09-20 19:34:55 +0800

    add CalculatorApp template

---
+++ project-2/CalculatorApp/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Yuhsuan Lin on 2025/9/20.
+//
+
+import Testing
+
+struct CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
